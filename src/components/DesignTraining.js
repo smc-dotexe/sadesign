58e01c06851414f3e2620a60ad65b2422ed75abd
Shane McGuire <shanelgmcguire@gmail.com>
2019-07-24 16:30:02 -0600

    added design training component

--- src/components/DesignTraining.js
+++ src/components/DesignTraining.js
@@ -4,6 +4,18 @@ class DesignTraining extends React.Component {
     render() {
         return (
             <div className='slide designTraining'>
+                <div id='dtDescriptorDiv'>
+                    <h3>Design Training</h3>
+                    <p>Training like a champion requires <br/>
+                    discipline, dedication and design. Rumble <br/>
+                    Boxing has been the Freddie Roach <br/>
+                    training experience. It has honed a <br/>
+                    strategic marketing mind, a team player <br/>
+                    attitude and a technical design hand <br/>
+                    comparable to Ali.</p>
+                    <div id='arrowBlock'></div>
+                    <h3>Rumble</h3>
+                </div>
             </div>
         )
     }
