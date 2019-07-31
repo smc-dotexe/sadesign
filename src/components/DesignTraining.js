61459995824f0019378885e3d120d1550487fd7d
Shane McGuire <shanelgmcguire@gmail.com>
2019-07-30 23:18:00 -0600

    added function to slideshow

--- src/components/DesignTraining.js
+++ src/components/DesignTraining.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import rumbleGram1 from '.././images/rumble-gram1.png'
+import rumbleGram2 from '.././images/rumblegram2.jpg'
 
 class DesignTraining extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            slideshow: false,
+        }
+    }
+
+    slideshow = () => {
+        this.setState(prevState => ({slideshow:!prevState.slideshow}))
+    }
+
     render() {
         return (
             <div className='slide designTraining'>
@@ -18,18 +30,28 @@ class DesignTraining extends React.Component {
                     <div id='arrowBlock'></div>
                     <h3>RUMBLE CASE STUDY</h3>
                 </div>
-                <div id='rumbleSlideShow'>
-                    <div className='slideShowImage'>
-                        <img id='rumbleGram1' src={rumbleGram1} alt='rumble1' />
-                    </div>
-                    <div className='slideShowImage'>
-                    </div>
-                    <div className='slideShowImage'>
-                    </div>
+                <div id='rumbleSlideShow' onClick={this.slideshow}>
+                    {this.state.slideshow ? 
+                        <div className='slideShowImage'>
+                            <img id='rumbleGram1' src={rumbleGram1} alt='rumble1' />
+                        </div> : 
+                        <div className='slideShowImage'>
+                            <img id='rumbleGram2' src={rumbleGram2} alt='rumble2' />
+                        </div>
+                    }
                     <div>
+                    {this.state.slideshow ? 
+                        <span className='dot'></span> :
+                        <span className='dotClear'></span>
+                    }
+
+                    {this.state.slideshow ?
+                        <span className='dotClear'></span> :
+                        <span className='dot'></span>
+                    }
+                        {/* <span className="dot"></span> 
                         <span className="dot"></span> 
-                        <span className="dot"></span> 
-                        <span className="dot"></span> 
+                        <span className="dot"></span>  */}
                     </div>
                 </div>
             </div>
