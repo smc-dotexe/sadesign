4b82ba7c489dde91c8d36910330b172fdca06d59
Shane McGuire <shanelgmcguire@gmail.com>
2019-09-08 00:28:07 -0600

    adding responsiveness too second section of intro page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './animations.css';
-import './mediaQuery.css'
+import './styles/introQuery.css'
 import Intro from './components/Intro'
 import DesignersArt from './components/DesignersArt'
 import DesignTraining from './components/DesignTraining'
--- src/components/DesignersArt.js
+++ src/components/DesignersArt.js
@@ -1,35 +1,34 @@
 import React from 'react'
-import greyArrow from '../images/greyarrowright.png'
 import redArrow from '../images/redarrowright.png'
+import '../styles/designersArtQuery.css'
 
 class DesignersArt extends React.Component {
     render() {
         return (
             <div className='slide designersArt'>
                 <div id='border'></div>
-                <h1>THE DESIGNERS ART</h1>
-                    <p>
-                        I aim to improve the world around me through human <br/>
-                        centred design. I approach projects with empathy and <br/>
-                        intuition. Equipped with a thirst for information and a <br/>
-                        knack for research I tackle tough design challenges with <br/>
-                        an ambition to produce good design and a need to <br/>
-                        make an impact.
+                <h1 id='goodDesignTitle'>Good Design</h1>
+                <h1 id='daTitle'>THE DESIGNER'S ART</h1>
+                    <p id='daTitleDescription'>
+                        I aim to improve the world around me through human 
+                        centred design. I approach projects with empathy and 
+                        intuition.
                     </p>
-                    <img id='arr1' src={greyArrow} alt='grey arrow' />
-                <div id='daBG'>
-                    <h1>"If you want <br/>
-                        momentum, you'll <br/>
-                        have to create it"
-                    </h1>
-                </div>
+                    <img id='arr1' src={redArrow} alt='red arrow' />
+                <div className='slide'>
+                    <div id='daBG'>
+                        <h1>"If you want <br/>
+                            momentum, you'll <br/>
+                            have to create it"
+                        </h1>
+                    
                     <p id='daAuthor'> &#8212; Ryan Holiday, Author of The Obstacle Is The Way, 2014.</p>
-                    <p id='buildBrand'>BUILDING MY BRAND, MY VOICE</p>
                     {/* <p id='arr2'>
                         >
                     </p> */}
                     <img id='arr2' src={redArrow} alt='redarrow' />
-
+                    </div>
+                </div>
             </div>
         )
     }
