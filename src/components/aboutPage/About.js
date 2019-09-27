3a30db2125df2cd5fa722f242bf14171fea36717
Shane McGuire <shanelgmcguire@gmail.com>
2019-09-26 18:55:26 -0600

    setup basic layout of about page

--- src/components/aboutPage/About.js
+++ src/components/aboutPage/About.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './about.css'
+import HeaderComp from '../headerComp/HeaderComp'
+import test from '../../images/pdb.png'
 
 class About extends React.Component {
     render() {
     return (
         <div className='slide aboutDiv'>
-            <h1>About Page</h1>
+            <HeaderComp passBgImage={test}/>
+            <h1 className='aboutDesignersArt'>
+                The Designers Art
+            </h1>
+            <p className='aboutDescription'>
+            I aim to improve the world around me through human centred design.
+            I approach projects with empathy and intuition. Equipped with a thirst
+            for information and a knack for research I tackle tough design
+            challenges with an ambition to produce good design and a need to 
+            make an impact.
+            </p>
         </div>
     )
     }
---
+++ src/components/headerComp/HeaderComp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './headerstyles.css'
+
+function HeaderComp(props) {
+
+    const bgImage = props.passBgImage
+    return (
+        <div 
+            className='headerDiv'
+            // style={{backgroundImage:`url(${bgImage})`}}
+        >
+            <header>
+                <h1 className='sarah'>SARAH</h1>
+                <h1 className='alonso'>ALONSO</h1>
+                <h1 className='design'>DESIGN</h1>
+            </header>
+
+        </div>
+    )
+}
+
+export default HeaderComp
