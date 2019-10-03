ed0f940a244351526fde2793a1d776382cdaeb14
Shane McGuire <shanelgmcguire@gmail.com>
2019-10-02 21:21:27 -0600

    added renu preview to work section

--- src/components/workPage/WorkList.js
+++ src/components/workPage/WorkList.js
@@ -3,12 +3,15 @@ import './worklist.css'
 import './responsiveWorkList.css'
 import PortfolioPreview from './portfolioPreview/portfolioPreview'
 import workForMySelfImage from '../../images/workForMyselfImage.png'
+import renuImage from '../../images/renuPreview.png'
 
 
 class WorkPage extends React.Component {
     render() {
         let workForMyselfDescription = 
             'As a Designer sometimes we fail to design for ourselves. We work oftentimes to build platforms for others to stand out. I thought... "What about me?"'
+        let renuDescription = 
+            'The Branding for Renu - Health and Wellness sought to create an icon that represented the yin and the yang of the body moving amongst Lymph.'
         return (
             <div className='workDiv'>
                 <h1 className='workPageHeading'>
@@ -21,6 +24,14 @@ class WorkPage extends React.Component {
                             passPreviewHeading='WORK FOR MYSELF'
                             passPreviewImage={workForMySelfImage}
                             passPreviewDescription={workForMyselfDescription}
+                        />
+                    </li>
+                    <li>
+                        <PortfolioPreview 
+                            passDateHeading='03.19'
+                            passPreviewHeading='RENU YOUR HEALTH'
+                            passPreviewImage={renuImage}
+                            passPreviewDescription={renuDescription}
                         />
                     </li>
                 </ul>
