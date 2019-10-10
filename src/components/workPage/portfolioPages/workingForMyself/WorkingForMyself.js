6ec9c8c68a3d701b5e2e321386778f4c523088af
Shane McGuire <shanelgmcguire@gmail.com>
2019-10-10 15:20:33 -0600

    refactored wfm page code

--- src/components/workPage/portfolioPages/workingForMyself/WorkingForMyself.js
+++ src/components/workPage/portfolioPages/workingForMyself/WorkingForMyself.js
@@ -15,19 +15,23 @@ class WorkingForMyself extends React.Component {
         super()
             this.state = {
                 counter: 0,
+                imageList: [wfmImage1, wfmImage2, wfmImage3, wfmImage4, wfmImage5]
             }
     }
 
     imageChange = () => {
         if(this.state.counter >= 4) {
-            this.setState({counter: 0}, ()=> {console.log('if counter=', this.state.counter)})
+            this.setState({counter: 0})
         } else {
-            this.setState({counter: this.state.counter + 1}, ()=> {console.log('else counter', this.state.counter)})
+            this.setState({counter: this.state.counter + 1})
         }
     }
 
     render() {
         const imageNo = this.state.counter
+        const imageList = this.state.imageList
+        let imageIndex
+
         return(
             <div className='wfmContainer'>
             <div className='slide workingForMyselfDiv'>
@@ -49,51 +53,30 @@ class WorkingForMyself extends React.Component {
                     of a skilled technician.
                 </p>
                 <div className='wfmImageContainer' onClick={this.imageChange}>
-                    {imageNo===0 ? 
-                        <img className='wfmImage' id='wfmImage1' src={wfmImage1} alt="cards on mirror" /> :
-                        <img className='wfmImageDisplayNone' alt='none' />
-                    }
-                    {imageNo===1 ? 
-                        <img className='wfmImage' id='wfmImage2' src={wfmImage2} alt="cards on mirror" /> :
-                        <img className='wfmImageDisplayNone' alt='none' />
-                    }
-                    {imageNo===2 ? 
-                        <img className='wfmImage' id='wfmImage3' src={wfmImage3} alt="cards on mirror" /> :
-                        <img className='wfmImageDisplayNone' alt='none' />
-                    }
-                    {imageNo===3 ? 
-                        <img className='wfmImage' id='wfmImage4' src={wfmImage4} alt="cards on mirror" /> :
-                        <img className='wfmImageDisplayNone' alt='none' />
-                    }
-                    {imageNo===4 ? 
-                        <img className='wfmImage' id='wfmImage5' src={wfmImage5} alt="cards on mirror" /> :
-                        <img className='wfmImageDisplayNone' alt='none' />
-                    }
+
+                    <img 
+                        className='wfmImage' 
+                        id={`wfmImage${imageNo}`} 
+                        src={imageList[imageNo]} 
+                        alt='working for myself image' />
                     <p className='wfmThanks'>
                     - Special thanks to Iriving Crea for helping me 
                     capture the vision I have for myself and my brand.
-                </p>
+                    </p>
+
                 <div className='dotContainer'>
-                    {imageNo===0 ?
-                        <span className='dot'></span>:
-                        <span className='dotClear'></span>
-                    }
-                    {imageNo===1 ?
-                        <span className='dot'></span>:
-                        <span className='dotClear'></span>
-                    }
-                    {imageNo===2 ?
-                        <span className='dot'></span>:
-                        <span className='dotClear'></span>
-                    }
-                    {imageNo===3 ?
-                        <span className='dot'></span>:
-                        <span className='dotClear'></span>
-                    }
-                    {imageNo===4 ?
-                        <span className='dot'></span>:
-                        <span className='dotClear'></span>
-                    }
+                    {imageList.map(imageDot => {
+                        imageIndex=imageList.indexOf(imageDot)
+                        if(imageIndex === imageNo) {
+                            return (
+                                <span key={imageNo} className='dot'></span>
+                            )
+                        }
+                            return (
+                                <span key={imageIndex} className='dotClear'></span>
+                            )
+                    })}
+
                 
                 </div>
                 </div>
