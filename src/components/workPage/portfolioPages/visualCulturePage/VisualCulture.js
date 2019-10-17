dd836a8adea4e90d313d8fb9fcdf1c31a1999808
Shane McGuire <shanelgmcguire@gmail.com>
2019-10-17 16:17:14 -0600

    added more things to vc workpage

--- src/components/workPage/portfolioPages/visualCulturePage/VisualCulture.js
+++ src/components/workPage/portfolioPages/visualCulturePage/VisualCulture.js
@@ -16,6 +16,16 @@ class VisualCulture extends React.Component {
 
                 <div className='vcDetails'>
                     <h1 className='vcHeading'>5 People Average</h1>
+                    <p className='timFerrissQuote'>
+                        "you are the average of the five people you associate
+                        with most. So do not underestimate the effects of your 
+                        pessimistic, unambitious, or disorganized friends. If 
+                        someone isn't making you stronger,"<br/>
+                        <span className='timFerrissCaption'>
+                        - Tim Ferriss, author of The 4-hour Work Week
+                        </span>
+                    </p>
+
                 </div>
             </div>
         )
