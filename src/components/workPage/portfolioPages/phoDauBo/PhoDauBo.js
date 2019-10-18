c420aa9d2f9bdf5ccc7289021ced259bf7071dc3
Shane McGuire <shanelgmcguire@gmail.com>
2019-10-18 00:42:12 -0600

    mostly completed responsiveness to pdb page

--- src/components/workPage/portfolioPages/phoDauBo/PhoDauBo.js
+++ src/components/workPage/portfolioPages/phoDauBo/PhoDauBo.js
@@ -9,6 +9,10 @@ import pdbMenuFoldout2 from '../../../../images/pdbMenuFoldOut2.png'
 import pdbLayout from '../../../../images/pdbLayout.png'
 import vietKitchen2 from '../../../../images/pdbVietKitchen2.svg'
 import vietKitchenMobile from '../../../../images/pdbVietKitchenMobile.svg'
+import pdbMenuFoldoutMobile from '../../../../images/pdbMenuFoldOut_Mobile.png'
+import pdbMenuFoldout2Mobile from '../../../../images/pdbMenuFoldOut2_Mobile.png'
+import pdbMenuFoldoutTablet from '../../../../images/pdbMenuFoldOut_Tablet.png'
+import pdbMenuFoldout2Tablet from '../../../../images/pdbMenuFoldOut2_Tablet.png'
 
 
 
@@ -36,22 +40,30 @@ class PhoDauBo extends React.Component {
                     </p>
                     <img className='vietKitchen' src={vietKitchen} alt='vietnamese kitchen label' />
                     <img className='vietKitchenMobile' src={vietKitchenMobile} alt='vietnamese kitchen label' />
-                    <h2 className='onTheMenuHeading'>ON THE MENU FOR DESIGN</h2>
-                    <ul className='designMenuList1'>
-                        <li>Logo Design</li>
-                        <li>Research</li>
-                        <li>Strategy</li>
-                        <li>Digital Graphics</li>
-                        <li>Print Material Designs</li>
-                    </ul>
-                    <ul className='designMenuList2'>
-                        <li>Main Designs - Main Menu, <br/>Take-out Menu and Dessert</li>
-                        <li>Business Cards</li>
-                        <li>Outdoor Signage</li>
-                    </ul>
+                    <div className='onTheMenuDiv'>
+                        <h2 className='onTheMenuHeading'>WHAT'S ON THE MENU FOR DESIGN</h2>
+                        <ul className='designMenuList1'>
+                            <li>Logo Design</li>
+                            <li>Retail Design</li>
+                            <li>Sticker Design</li>
+                            <li>Creation of Digital and Print Assets</li>
+                            <li>Print Material Designs</li>
+                        </ul>
+                        <ul className='designMenuList2'>
+                            <li>Main Designs - Main Menu, <br/>Take-out Menu and Dessert</li>
+                            <li>Business Cards</li>
+                            <li>Outdoor Signage</li>
+                        </ul>
+                    </div>
                     <img className='pdbMenu1' src={pdbMenuFoldout} alt='menu foldout' />
+                    <img className='pdbMenu1_Mobile' src={pdbMenuFoldoutMobile} alt='menu foldout' />
+                    {/* <img className='pdbMenu1_Tablet' src={pdbMenuFoldoutTablet} alt='menu foldout' /> */}
                     <h1 className='pdbHeading2'>PHO DAU BO</h1>
                     <img className='pdbMenu2' src={pdbMenuFoldout2} alt='menu foldout 2' />
+                    <img className='pdbMenu2_Mobile' src={pdbMenuFoldout2Mobile} alt='menu foldout' />
+                    <img className='pdbMenu2_Tablet' src={pdbMenuFoldout2Tablet} alt='menu foldout' />
+
+
                     <p className='pdbMenuDescription'>
                         The biggest challenge of this project was to condense
                         an 8-page-slip-cover-flip-menu into a modern one page menu.
